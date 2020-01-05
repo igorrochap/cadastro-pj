487a4865395a8d0065c7f731e7db2d56dd3a3b28
igorrochap <igor.systemof@gmail.com>
2020-01-05 16:48:17 -0300

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('body')
+    <div class="jumbotron bg-light border border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Produtos</h5>
+                        <p class="card-text">
+                            Aqui você cadastra seus produtos.
+                            Não esqueça de cadastrar previamente suas categorias.
+                        </p>
+                        <a href="#" class="btn btn-primary">Cadastre seus produtos</a>
+                    </div>
+                </div>
+
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Categorias</h5>
+                        <p class="card-text">
+                            Cadaste as categorias dos seus produtos.
+                        </p>
+                        <a href="#" class="btn btn-primary">Cadastre suas Categorias</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Cadastro de Produtos</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <style>
+        body{
+            padding: 10px;
+        }
+
+        .navbar{
+            margin-bottom: 10px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        @component('components.navbar')
+            
+        @endcomponent
+        <main role="main">
+            @hasSection ('body')
+                @yield('body')                
+            @endif
+        </main>
+    </div>
+
+
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
