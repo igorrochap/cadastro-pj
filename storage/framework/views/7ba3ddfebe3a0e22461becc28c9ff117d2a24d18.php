40093795de99e931c892684e2f0f42e3afd8d0bd
Igor R <igor.systemof@gmail.com>
2020-01-06 08:00:29 -0300

    Putting models into folder Models

---
+++ storage/framework/views/7ba3ddfebe3a0e22461becc28c9ff117d2a24d18.php
@@ -0,0 +1,29 @@
+<?php $__env->startSection('body'); ?>
+    <div class="jumbotron bg-light border border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Produtos</h5>
+                        <p class="card-text">
+                            Aqui você cadastra seus produtos.
+                            Não esqueça de cadastrar previamente suas categorias.
+                        </p>
+                        <a href="#" class="btn btn-primary">Cadastre seus produtos</a>
+                    </div>
+                </div>
+
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de Categorias</h5>
+                        <p class="card-text">
+                            Cadaste as categorias dos seus produtos.
+                        </p>
+                        <a href="#" class="btn btn-primary">Cadastre suas Categorias</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/estagiariodev/Documentos/Igor/projetos/cadastro-pj/resources/views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f899d7e2d92b5cece30b3b0e7a18dd7dd7f5ec69.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <title>Cadastro de Produtos</title>
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+
+    <style>
+        body{
+            padding: 10px;
+        }
+
+        .navbar{
+            margin-bottom: 10px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <?php $__env->startComponent('components.navbar'); ?>
+            
+        <?php echo $__env->renderComponent(); ?>
+        <main role="main">
+            <?php if (! empty(trim($__env->yieldContent('body')))): ?>
+                <?php echo $__env->yieldContent('body'); ?>                
+            <?php endif; ?>
+        </main>
+    </div>
+
+
+    <script src="<?php echo e(asset('js/app.js')); ?>" type="text/javascript"></script>
+</body>
+</html><?php /**PATH /home/estagiariodev/Documentos/Igor/projetos/cadastro-pj/resources/views/layouts/app.blade.php ENDPATH**/ ?>
